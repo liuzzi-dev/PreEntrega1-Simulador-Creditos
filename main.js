e343c4319395b2c416770b44072c43ba323f5731
Axel Liuzzi <liuzzi.dev@gmail.com>
2023-10-20 22:06:27 -0300

    Se agrego validacion sobre los datos que ingresa el usuario

--- main.js
+++ main.js
@@ -2,14 +2,31 @@ function simuladorPrestamo() {
     let continuar;
 
     do {
-        // Se obtener el monto del préstamo del usuario
-        const montoPrestamo = parseFloat(prompt("Ingrese el monto del préstamo:"));
+        let montoPrestamo, tasaInteresAnual, plazoPrestamoMeses;
 
-        // Se obtener la tasa de interés anual del usuario
-        const tasaInteresAnual = parseFloat(prompt("Ingrese la tasa de interés anual (%):")) / 100;
+        // Se obtiene el monto del préstamo del usuario con validación
+        do {
+            montoPrestamo = parseFloat(prompt("Ingrese el monto del préstamo:"));
+            if (isNaN(montoPrestamo) || montoPrestamo <= 0) {
+                alert('Por favor, ingrese un monto válido y positivo.');
+            }
+        } while (isNaN(montoPrestamo) || montoPrestamo <= 0);
 
-        // Se obtener el plazo del préstamo en meses
-        const plazoPrestamoMeses = parseInt(prompt("Ingrese el plazo del préstamo en meses:"));
+        // Se obtiene la tasa de interés anual del usuario con validación
+        do {
+            tasaInteresAnual = parseFloat(prompt("Ingrese la tasa de interés anual (%):")) / 100;
+            if (isNaN(tasaInteresAnual) || tasaInteresAnual <= 0) {
+                alert('Por favor, ingrese una tasa de interés válida y positiva.');
+            }
+        } while (isNaN(tasaInteresAnual) || tasaInteresAnual <= 0);
+
+        // Se obtiene el plazo del préstamo en meses con validación
+        do {
+            plazoPrestamoMeses = parseInt(prompt("Ingrese el plazo del préstamo en meses:"));
+            if (isNaN(plazoPrestamoMeses) || plazoPrestamoMeses <= 0) {
+                alert('Por favor, ingrese un plazo válido y positivo en meses.');
+            }
+        } while (isNaN(plazoPrestamoMeses) || plazoPrestamoMeses <= 0);
 
         // Se calcula la tasa de interés mensual
         const tasaInteresMensual = tasaInteresAnual / 12;
@@ -20,7 +37,7 @@ function simuladorPrestamo() {
         // Se muestra la cuota mensual al usuario
         alert(`La cuota mensual es de $${cuotaMensual.toFixed(2)}`);
 
-        // Se consulta si desde realizar otro calculo al usuario
+        // Se consulta si desea realizar otro cálculo al usuario
         continuar = prompt("¿Desea realizar otro cálculo? (Si/No)");
 
         // Se muestra mensaje de cierre de simulador
